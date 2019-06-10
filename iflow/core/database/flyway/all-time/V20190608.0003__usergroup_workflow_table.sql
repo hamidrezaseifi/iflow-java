1ea9f2eb2ff944c95ed568546d3f8fd8499a8364
hamidrezaseifi-at-513711302859 <hamidrezaseifi@gmail.com>
2019-06-10 20:42:29 +0200

    core: new tables

--- iflow/core/database/flyway/all-time/V20190608.0003__usergroup_workflow_table.sql
+++ iflow/core/database/flyway/all-time/V20190608.0003__usergroup_workflow_table.sql
@@ -57,6 +57,55 @@ CREATE TABLE `workflow_step` (
   CONSTRAINT `FK_WORKFLOWSTEP_WORKFLOW` FOREIGN KEY (`workflow_id`) REFERENCES `workflow` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB;
 
+DROP TABLE IF EXISTS `departments`;
+CREATE TABLE `departments` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `companyid` int(11) NOT NULL DEFAULT '1',
+  `title` varchar(200) NOT NULL,
+  `status` smallint(6) NOT NULL DEFAULT '1',
+  `version` int(11) NOT NULL DEFAULT '1',
+  `created_at` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+  `updated_at` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+  PRIMARY KEY (`id`),
+  KEY `FK_DEPARTMENTS_COMPANY_idx` (`companyid`),
+  CONSTRAINT `FK_DEPARTMENTS_COMPANY` FOREIGN KEY (`companyid`) REFERENCES `companies` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB;
 
+
+DROP TABLE IF EXISTS `user_departments`;
+CREATE TABLE `user_departments` (
+  `user_id` int(11) NOT NULL,
+  `department_id` int(11) NOT NULL,
+  `created_at` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+  PRIMARY KEY (`user_id`,`department_id`),
+  KEY `FK_USERDEPARTMENTS_DEPARTMENTS_idx` (`department_id`),
+  CONSTRAINT `FK_USERDEPARTMENTS_DEPARTMENTS` FOREIGN KEY (`department_id`) REFERENCES `departments` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `FK_USERDEPARTMENTS_USERS` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB;
+
+DROP TABLE IF EXISTS `departments_group`;
+CREATE TABLE `departments_group` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `department_id` int(11) NOT NULL,
+  `title` varchar(200) NOT NULL,
+  `status` smallint(6) NOT NULL DEFAULT '1',
+  `version` int(11) NOT NULL DEFAULT '1',
+  `created_at` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+  `updated_at` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+  PRIMARY KEY (`id`),
+  KEY `FK_DEPARTMENTGROUP_DEPARTMENT_idx` (`department_id`),
+  CONSTRAINT `FK_DEPARTMENTGROUP_DEPARTMENT` FOREIGN KEY (`department_id`) REFERENCES `departments` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB;
+
+DROP TABLE IF EXISTS `user_deputy`;
+CREATE TABLE `user_deputy` (
+  `user_id` int(11) NOT NULL,
+  `deputy_id` int(11) NOT NULL,
+  `created_at` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+  PRIMARY KEY (`user_id`,`deputy_id`),
+  KEY `FK_USERDEPUTY_DEPUTY_idx` (`deputy_id`),
+  CONSTRAINT `FK_USERDEPUTY_DEPUTY` FOREIGN KEY (`deputy_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `FK_USERDEPUTY_USER` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB;
 
 
