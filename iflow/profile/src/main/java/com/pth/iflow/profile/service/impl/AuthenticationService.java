d07b59055f3b0696e1c759ca643ab7b6e8ec6c2b
hamidrezaseifi-at-513711302859 <hamidrezaseifi@gmail.com>
2019-08-29 10:37:53 +0200

    authentication for user 2 & 3

--- iflow/profile/src/main/java/com/pth/iflow/profile/service/impl/AuthenticationService.java
+++ iflow/profile/src/main/java/com/pth/iflow/profile/service/impl/AuthenticationService.java
@@ -15,7 +15,7 @@ import com.pth.iflow.profile.service.IAuthenticationService;
 @Service
 public class AuthenticationService implements IAuthenticationService {
 
-  private static final Logger logger = LoggerFactory.getLogger(UsersService.class);
+  private static final Logger       logger                = LoggerFactory.getLogger(UsersService.class);
 
   private final Map<String, String> tempEmailPasswordList = new HashMap<>();
 
@@ -24,20 +24,20 @@ public class AuthenticationService implements IAuthenticationService {
 
   @PostConstruct
   public void init() {
-    tempEmailPasswordList.put("admin@iflow.de", "admin");
-    tempEmailPasswordList.put("user@iflow.de", "user");
-    tempEmailPasswordList.put("eli", "eli");
-    tempEmailPasswordList.put("test", "test");
+    this.tempEmailPasswordList.put("admin@iflow.de", "admin");
+    this.tempEmailPasswordList.put("user@iflow.de", "user");
+    this.tempEmailPasswordList.put("user2@iflow.de", "user");
+    this.tempEmailPasswordList.put("user3@iflow.de", "user");
+    this.tempEmailPasswordList.put("test", "test");
   }
 
   @Override
   public UserAuthenticationRequest authenticate(final UserAuthenticationRequest user) {
 
+    logger.debug("Authenticating user {}", user.getEmail());
 
-    logger.debug("Authenticating user {}" , user.getEmail());
-
-    if (tempEmailPasswordList.keySet().contains(user.getEmail())) {
-      if (tempEmailPasswordList.get(user.getEmail()).equals(user.getPassword())) {
+    if (this.tempEmailPasswordList.keySet().contains(user.getEmail())) {
+      if (this.tempEmailPasswordList.get(user.getEmail()).equals(user.getPassword())) {
         return user;
       }
     }
