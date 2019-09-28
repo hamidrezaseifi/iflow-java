70316e8f45d75db713d731ca98ff6b462dd76d68
hamidrezaseifi-at-513711302859 <hamidrezaseifi@gmail.com>
2019-09-28 11:58:23 +0200

    access to profile items

--- iflow/gui/src/main/java/com/pth/iflow/gui/models/ui/GuiSessionUserInfo.java
+++ iflow/gui/src/main/java/com/pth/iflow/gui/models/ui/GuiSessionUserInfo.java
@@ -17,8 +17,11 @@ import org.springframework.web.context.annotation.SessionScope;
 
 import com.pth.iflow.common.exceptions.IFlowMessageConversionFailureException;
 import com.pth.iflow.gui.exceptions.GuiCustomizedException;
+import com.pth.iflow.gui.models.GuiCompany;
 import com.pth.iflow.gui.models.GuiCompanyProfile;
+import com.pth.iflow.gui.models.GuiDepartment;
 import com.pth.iflow.gui.models.GuiUser;
+import com.pth.iflow.gui.models.GuiUserGroup;
 import com.pth.iflow.gui.models.GuiWorkflow;
 import com.pth.iflow.gui.models.GuiWorkflowType;
 import com.pth.iflow.gui.services.IUserAccess;
@@ -37,11 +40,12 @@ public class GuiSessionUserInfo {
   private GuiCompanyProfile                companyProfile;
   private String                           token;
   private String                           sessionId;
+
   private final Map<Long, GuiWorkflow>     cachedWorkflow             = new HashMap<>();
 
-  private final Map<Long, GuiUser>         userMap                    = new HashMap<>();
+  private final Map<Long, GuiUser>         companyUsers               = new HashMap<>();
 
-  private final Map<Long, GuiWorkflowType> workflowTypeMap            = new HashMap<>();
+  private final Map<Long, GuiWorkflowType> comapnyWorkflowTypes       = new HashMap<>();
 
   @Value("${server.session.timeout}")
   private int                              sessionTimeOut;
@@ -130,38 +134,38 @@ public class GuiSessionUserInfo {
   }
 
   /**
-   * @return the userMap
+   * @return the companyUsers
    * @throws IFlowMessageConversionFailureException
    */
-  public Map<Long, GuiUser> getUserMap() throws IFlowMessageConversionFailureException {
-    if (this.userMap.size() == 0) {
+  public Map<Long, GuiUser> getCompanyUsers() throws IFlowMessageConversionFailureException {
+    if (this.companyUsers.size() == 0) {
       try {
         final List<GuiUser> userList = this.userAccess.getCompanyUserList(this.companyProfile.getCompany().getId());
-        this.userMap.putAll(userList.stream().collect(Collectors.toMap(u -> u.getId(), u -> u)));
+        this.companyUsers.putAll(userList.stream().collect(Collectors.toMap(u -> u.getId(), u -> u)));
       } catch (GuiCustomizedException | MalformedURLException e) {
-        logger.error("error in reading user list: {} \n {}", e.getMessage(), e);
+        logger.error("error in reading company user list: {} \n {}", e.getMessage(), e);
       }
     }
 
-    return this.userMap;
+    return this.companyUsers;
   }
 
   /**
    * @return the workflowTypeMap
    * @throws IFlowMessageConversionFailureException
    */
-  public Map<Long, GuiWorkflowType> getWorkflowTypeMap() throws IFlowMessageConversionFailureException {
-    if (this.workflowTypeMap.size() == 0) {
+  public Map<Long, GuiWorkflowType> getCompanyWorkflowTypes() throws IFlowMessageConversionFailureException {
+    if (this.comapnyWorkflowTypes.size() == 0) {
       try {
         final List<GuiWorkflowType> typeList = this.workflowAccess.readWorkflowTypeList(this.companyProfile.getCompany().getId(),
             this.getToken());
-        this.workflowTypeMap.putAll(typeList.stream().collect(Collectors.toMap(t -> t.getId(), t -> t)));
+        this.comapnyWorkflowTypes.putAll(typeList.stream().collect(Collectors.toMap(t -> t.getId(), t -> t)));
       } catch (GuiCustomizedException | MalformedURLException e) {
-        logger.error("error in reading user list: {} \n {}", e.getMessage(), e);
+        logger.error("error in reading company workflowtype list: {} \n {}", e.getMessage(), e);
       }
     }
 
-    return this.workflowTypeMap;
+    return this.comapnyWorkflowTypes;
   }
 
   /**
@@ -169,17 +173,17 @@ public class GuiSessionUserInfo {
    * @throws IFlowMessageConversionFailureException
    */
   public GuiWorkflowType getWorkflowTypeById(final Long workflowTypId) throws IFlowMessageConversionFailureException {
-    if (this.workflowTypeMap.size() == 0) {
+    if (this.comapnyWorkflowTypes.size() == 0) {
       try {
         final List<GuiWorkflowType> typeList = this.workflowAccess.readWorkflowTypeList(this.companyProfile.getCompany().getId(),
             this.getToken());
-        this.workflowTypeMap.putAll(typeList.stream().collect(Collectors.toMap(t -> t.getId(), t -> t)));
+        this.comapnyWorkflowTypes.putAll(typeList.stream().collect(Collectors.toMap(t -> t.getId(), t -> t)));
       } catch (GuiCustomizedException | MalformedURLException e) {
-        logger.error("error in reading user list: {} \n {}", e.getMessage(), e);
+        logger.error("error in reading company workflowtype: {} \n {}", e.getMessage(), e);
       }
     }
 
-    return this.workflowTypeMap.containsKey(workflowTypId) ? this.workflowTypeMap.get(workflowTypId) : null;
+    return this.comapnyWorkflowTypes.containsKey(workflowTypId) ? this.comapnyWorkflowTypes.get(workflowTypId) : null;
   }
 
   /**
@@ -187,16 +191,16 @@ public class GuiSessionUserInfo {
    * @throws IFlowMessageConversionFailureException
    */
   public GuiUser getUserById(final Long userId) throws IFlowMessageConversionFailureException {
-    if (this.userMap.size() == 0) {
+    if (this.companyUsers.size() == 0) {
       try {
         final List<GuiUser> userList = this.userAccess.getCompanyUserList(this.companyProfile.getCompany().getId());
-        this.userMap.putAll(userList.stream().collect(Collectors.toMap(u -> u.getId(), u -> u)));
+        this.companyUsers.putAll(userList.stream().collect(Collectors.toMap(u -> u.getId(), u -> u)));
       } catch (GuiCustomizedException | MalformedURLException e) {
         logger.error("error in reading user list: {} \n {}", e.getMessage(), e);
       }
     }
 
-    return this.userMap.containsKey(userId) ? this.userMap.get(userId) : null;
+    return this.companyUsers.containsKey(userId) ? this.companyUsers.get(userId) : null;
   }
 
   // Map<Long, GuiWorkflow> cachedWorkflow
@@ -211,5 +215,17 @@ public class GuiSessionUserInfo {
 
   public GuiWorkflow getCachedWorkflow(final Long workflowId) {
     return this.cachedWorkflow.get(workflowId);
+  }
+
+  public GuiCompany getCompany() {
+    return this.companyProfile.getCompany();
+  }
+
+  public List<GuiDepartment> getCompanyDepartments() {
+    return this.companyProfile.getDepartments();
+  }
+
+  public List<GuiUserGroup> getCompanyUserGroups() {
+    return this.companyProfile.getUserGroups();
   }
 }
