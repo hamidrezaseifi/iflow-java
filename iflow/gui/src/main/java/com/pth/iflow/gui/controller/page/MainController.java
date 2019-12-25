55117cb8211450842e76192601da16ed81c73c78
hamidrezaseifi-at-513711302859 <hamidrezaseifi@gmail.com>
2019-12-26 00:10:38 +0100

    set dependencies and code to use tess4 ocr in gui

--- iflow/gui/src/main/java/com/pth/iflow/gui/controller/page/MainController.java
+++ iflow/gui/src/main/java/com/pth/iflow/gui/controller/page/MainController.java
@@ -7,14 +7,15 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.multipart.MultipartFile;
 
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
+
 @Controller
-@RequestMapping(value = "/")
 public class MainController {
 
   @ResponseStatus(HttpStatus.OK)
@@ -37,6 +38,21 @@ public class MainController {
     }
 
     return "ok";
+  }
+
+  @ResponseStatus(HttpStatus.OK)
+  @GetMapping(path = { "/testocr" })
+  @ResponseBody
+  public String testOcr() throws IllegalStateException, IOException, TesseractException {
+
+    final File      file      = new File("E:\\TestRechnung\\Unbenannt.png");
+    final Tesseract tesseract = new Tesseract();
+    tesseract.setDatapath("F://Softwares//Tess4J//tessdata");
+    tesseract.setLanguage("deu");
+    tesseract.setHocr(true);
+    final String res = tesseract.doOCR(file);
+
+    return res;
   }
 
 }
