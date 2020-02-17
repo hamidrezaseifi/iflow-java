1226e9a8fac5a45049524887c990c0fb7eb275a8
hamidrezaseifi-at-513711302859 <hamidrezaseifi@gmail.com>
2020-02-17 12:00:00 +0100

    create sql

---
+++ iflow/core/database/flyway/all-time/V20200217.0009__company_workflow_ocr_settings.sql
@@ -0,0 +1,32 @@
+
+--company_workflowtype_items_ocr_settings_seq
+
+CREATE SEQUENCE company_workflowtype_items_ocr_settings_seq;
+
+CREATE TABLE company_workflowtype_items_ocr_settings
+(
+    id bigint NOT NULL DEFAULT nextval('company_workflowtype_items_ocr_settings_seq'::regclass),
+    company_id bigint NOT NULL,
+    workflow_type_id bigint NOT NULL,
+    property_name character varying(20) COLLATE pg_catalog."default" NOT NULL,
+    value character varying(4000) COLLATE pg_catalog."default",
+    status smallint NOT NULL DEFAULT 1,
+    version integer NOT NULL DEFAULT 1,
+    created_at timestamp without time zone NOT NULL default (now() at time zone 'utc'),
+  	updated_at timestamp without time zone NOT NULL default (now() at time zone 'utc'),
+    CONSTRAINT company_workflowtype_items_ocr_settings_pkey PRIMARY KEY (id)
+        USING INDEX TABLESPACE iflow_tablespace,
+    CONSTRAINT "FK_COMPANY_WORKFLOWTYPE_ITEMS_OCR_SETTINGS_COMPANY" FOREIGN KEY (id)
+        REFERENCES public.companies (id) MATCH SIMPLE
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
+    CONSTRAINT "FK_COMPANY_WORKFLOWTYPE_ITEMS_OCR_SETTINGS_WORKFLOWTYPE" FOREIGN KEY (id)
+        REFERENCES public.workflow_type (id) MATCH SIMPLE
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+) ;
+
+ALTER SEQUENCE company_workflowtype_items_ocr_settings_seq OWNED BY company_workflowtype_items_ocr_settings.id;
+
+
+
